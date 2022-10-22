68971b8a47793605ce7e9ec40e0dcf688f73d58c
Aditya <adi454282@gmail.com>
2022-10-22 18:13:33 +0530

    Three basic pages built

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+
+
 class Home extends StatefulWidget {
-  const Home({super.key});
+  
 
   @override
-  State<Home> createState() => _HomeState();
+  _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      body: SafeArea(child:Text('Home Screen')),
     );
   }
 }
